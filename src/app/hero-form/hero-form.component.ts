114ac947a1d34c253cb9be9d506ae9dcb9cf572f
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-02-16 22:05:40 -0500

    🔥 Chore: Hero form component

--- src/app/hero-form/hero-form.component.ts
+++ src/app/hero-form/hero-form.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-hero-form',
   templateUrl: './hero-form.component.html',
-  styleUrls: ['./hero-form.component.css']
+  styleUrls: ['./hero-form.component.css'],
 })
-export class HeroFormComponent implements OnInit {
+export class HeroFormComponent {
+  powers = ['Really Smart', 'Super Flexible', 'Super Hot', 'Weather Changer'];
 
-  constructor() { }
+  model = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
 
-  ngOnInit(): void {
+  submitted = false;
+
+  onSubmit() {
+    this.submitted = true;
   }
-
 }
